96390d277cea84bcccdb5ba51c07a57ab42a0c14
Mike Vengerovich <mikeveng73@gmail.com>
2026-07-28 04:58:19 +0300

    + qspi psram 166 -> 133

--- src/Main.cpp
+++ src/Main.cpp
@@ -900,81 +900,83 @@ uint32_t __not_in_flash_func(butter_psram_size)() {
     BUTTER_PSRAM_SIZE = res << 20;
     return BUTTER_PSRAM_SIZE;
 }
-void __no_inline_not_in_flash_func(psram_init)(uint cs_pin) {
+/**
+ * Initialize PSRAM hardware with specified frequency.
+ * This function MUST run from RAM, not flash, as it reconfigures the XIP controller.
+ */
+void __no_inline_not_in_flash_func(psram_init_with_freq)(uint cs_pin, int freq_mhz) {
+    const int clock_hz = clock_get_hz(clk_sys);
+
+    // Configure GPIO for XIP CS1 function
     gpio_set_function(cs_pin, GPIO_FUNC_XIP_CS1);
 
-    // Enable direct mode, PSRAM CS, clkdiv of 10.
-    qmi_hw->direct_csr = 10 << QMI_DIRECT_CSR_CLKDIV_LSB | \
-                               QMI_DIRECT_CSR_EN_BITS | \
-                               QMI_DIRECT_CSR_AUTO_CS1N_BITS;
-    while (qmi_hw->direct_csr & QMI_DIRECT_CSR_BUSY_BITS)
-        ;
+    // Enter direct mode with slow clock for initialization
+    qmi_hw->direct_csr = 10 << QMI_DIRECT_CSR_CLKDIV_LSB |
+                        QMI_DIRECT_CSR_EN_BITS |
+                        QMI_DIRECT_CSR_AUTO_CS1N_BITS;
+    while (qmi_hw->direct_csr & QMI_DIRECT_CSR_BUSY_BITS);
 
-    // Enable QPI mode on the PSRAM
+    // Send QPI enable command (0x35) to PSRAM
     const uint CMD_QPI_EN = 0x35;
     qmi_hw->direct_tx = QMI_DIRECT_TX_NOPUSH_BITS | CMD_QPI_EN;
+    while (qmi_hw->direct_csr & QMI_DIRECT_CSR_BUSY_BITS);
 
-    while (qmi_hw->direct_csr & QMI_DIRECT_CSR_BUSY_BITS)
-        ;
+    // Calculate optimal clock divisor for target PSRAM frequency
+    const int max_psram_freq = freq_mhz * 1000000;
 
-    // Set PSRAM timing for APS6404
-    //
-    // Using an rxdelay equal to the divisor isn't enough when running the APS6404 close to 133MHz.
-    // So: don't allow running at divisor 1 above 100MHz (because delay of 2 would be too late),
-    // and add an extra 1 to the rxdelay if the divided clock is > 100MHz (i.e. sys clock > 200MHz).
-    const int max_psram_freq = 166000000;
-    const int clock_hz = clock_get_hz(clk_sys);
     int divisor = (clock_hz + max_psram_freq - 1) / max_psram_freq;
     if (divisor == 1 && clock_hz > 100000000) {
-        divisor = 2;
+        divisor = 2;  // Minimum divisor of 2 at high system clocks
     }
+
+    // RX delay compensation for high-speed operation
     int rxdelay = divisor;
     if (clock_hz / divisor > 100000000) {
         rxdelay += 1;
     }
 
-    // - Max select must be <= 8us.  The value is given in multiples of 64 system clocks.
-    // - Min deselect must be >= 18ns.  The value is given in system clock cycles - ceil(divisor / 2).
+    // Calculate timing parameters
     const int clock_period_fs = 1000000000000000ll / clock_hz;
-    const int max_select = (125 * 1000000) / clock_period_fs;  // 125 = 8000ns / 64
+    const int max_select_val = (125 * 1000000) / clock_period_fs;
     const int min_deselect = (18 * 1000000 + (clock_period_fs - 1)) / clock_period_fs - (divisor + 1) / 2;
 
-    qmi_hw->m[1].timing = 1 << QMI_M1_TIMING_COOLDOWN_LSB |
-                          QMI_M1_TIMING_PAGEBREAK_VALUE_1024 << QMI_M1_TIMING_PAGEBREAK_LSB |
-                          max_select << QMI_M1_TIMING_MAX_SELECT_LSB |
-                          min_deselect << QMI_M1_TIMING_MIN_DESELECT_LSB |
-                          rxdelay << QMI_M1_TIMING_RXDELAY_LSB |
-                          divisor << QMI_M1_TIMING_CLKDIV_LSB;
+    // Configure M1 (PSRAM) timing
+    qmi_hw->m[1].timing =
+        1 << QMI_M1_TIMING_COOLDOWN_LSB |
+        QMI_M1_TIMING_PAGEBREAK_VALUE_1024 << QMI_M1_TIMING_PAGEBREAK_LSB |
+        max_select_val << QMI_M1_TIMING_MAX_SELECT_LSB |
+        min_deselect << QMI_M1_TIMING_MIN_DESELECT_LSB |
+        rxdelay << QMI_M1_TIMING_RXDELAY_LSB |
+        divisor << QMI_M1_TIMING_CLKDIV_LSB;
 
-    // Set PSRAM commands and formats
+    // Configure read format: Quad mode, 0xEB fast read command, 6 dummy cycles
     qmi_hw->m[1].rfmt =
-        QMI_M0_RFMT_PREFIX_WIDTH_VALUE_Q << QMI_M0_RFMT_PREFIX_WIDTH_LSB |\
-        QMI_M0_RFMT_ADDR_WIDTH_VALUE_Q   << QMI_M0_RFMT_ADDR_WIDTH_LSB |\
-        QMI_M0_RFMT_SUFFIX_WIDTH_VALUE_Q << QMI_M0_RFMT_SUFFIX_WIDTH_LSB |\
-        QMI_M0_RFMT_DUMMY_WIDTH_VALUE_Q  << QMI_M0_RFMT_DUMMY_WIDTH_LSB |\
-        QMI_M0_RFMT_DATA_WIDTH_VALUE_Q   << QMI_M0_RFMT_DATA_WIDTH_LSB |\
-        QMI_M0_RFMT_PREFIX_LEN_VALUE_8   << QMI_M0_RFMT_PREFIX_LEN_LSB |\
-        6                                << QMI_M0_RFMT_DUMMY_LEN_LSB;
+        QMI_M0_RFMT_PREFIX_WIDTH_VALUE_Q << QMI_M0_RFMT_PREFIX_WIDTH_LSB |
+        QMI_M0_RFMT_ADDR_WIDTH_VALUE_Q << QMI_M0_RFMT_ADDR_WIDTH_LSB |
+        QMI_M0_RFMT_SUFFIX_WIDTH_VALUE_Q << QMI_M0_RFMT_SUFFIX_WIDTH_LSB |
+        QMI_M0_RFMT_DUMMY_WIDTH_VALUE_Q << QMI_M0_RFMT_DUMMY_WIDTH_LSB |
+        QMI_M0_RFMT_DATA_WIDTH_VALUE_Q << QMI_M0_RFMT_DATA_WIDTH_LSB |
+        QMI_M0_RFMT_PREFIX_LEN_VALUE_8 << QMI_M0_RFMT_PREFIX_LEN_LSB |
+        6 << QMI_M0_RFMT_DUMMY_LEN_LSB;
 
-    qmi_hw->m[1].rcmd = 0xEB;
+    qmi_hw->m[1].rcmd = 0xEB;  // Fast Read Quad I/O
 
+    // Configure write format: Quad mode, 0x38 write command
     qmi_hw->m[1].wfmt =
-        QMI_M0_WFMT_PREFIX_WIDTH_VALUE_Q << QMI_M0_WFMT_PREFIX_WIDTH_LSB |\
-        QMI_M0_WFMT_ADDR_WIDTH_VALUE_Q   << QMI_M0_WFMT_ADDR_WIDTH_LSB |\
-        QMI_M0_WFMT_SUFFIX_WIDTH_VALUE_Q << QMI_M0_WFMT_SUFFIX_WIDTH_LSB |\
-        QMI_M0_WFMT_DUMMY_WIDTH_VALUE_Q  << QMI_M0_WFMT_DUMMY_WIDTH_LSB |\
-        QMI_M0_WFMT_DATA_WIDTH_VALUE_Q   << QMI_M0_WFMT_DATA_WIDTH_LSB |\
-        QMI_M0_WFMT_PREFIX_LEN_VALUE_8   << QMI_M0_WFMT_PREFIX_LEN_LSB;
+        QMI_M0_WFMT_PREFIX_WIDTH_VALUE_Q << QMI_M0_WFMT_PREFIX_WIDTH_LSB |
+        QMI_M0_WFMT_ADDR_WIDTH_VALUE_Q << QMI_M0_WFMT_ADDR_WIDTH_LSB |
+        QMI_M0_WFMT_SUFFIX_WIDTH_VALUE_Q << QMI_M0_WFMT_SUFFIX_WIDTH_LSB |
+        QMI_M0_WFMT_DUMMY_WIDTH_VALUE_Q << QMI_M0_WFMT_DUMMY_WIDTH_LSB |
+        QMI_M0_WFMT_DATA_WIDTH_VALUE_Q << QMI_M0_WFMT_DATA_WIDTH_LSB |
+        QMI_M0_WFMT_PREFIX_LEN_VALUE_8 << QMI_M0_WFMT_PREFIX_LEN_LSB;
 
-    qmi_hw->m[1].wcmd = 0x38;
+    qmi_hw->m[1].wcmd = 0x38;  // Quad Write
 
-    // Disable direct mode
+    // Exit direct mode
     qmi_hw->direct_csr = 0;
 
-    // Enable writes to PSRAM
+    // Enable writes to M1 (PSRAM) region
     hw_set_bits(&xip_ctrl_hw->ctrl, XIP_CTRL_WRITABLE_M1_BITS);
-
-    butter_psram_size();
 }
 void __not_in_flash_func(sigbus)(void) {
     while(true) {
@@ -1103,7 +1105,7 @@ int main() {
     set_sys_clock_khz(CPU_MHZ * KHZ, 0);
     uint BUTTER_PSRAM_GPIO = rp2350a ? BUTTER_PSRAM_GPIO_RP2350A
                                       : BUTTER_PSRAM_GPIO_RP2350B;
-    psram_init(BUTTER_PSRAM_GPIO);
+    psram_init_with_freq(BUTTER_PSRAM_GPIO, 133);
     exception_set_exclusive_handler(HARDFAULT_EXCEPTION, sigbus);
 #else
     hw_set_bits(&vreg_and_chip_reset_hw->vreg, VREG_AND_CHIP_RESET_VREG_VSEL_BITS);
