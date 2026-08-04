64dbeab39c562ee2ca65a1c311302d0874a40a3b
Mike Vengerovich <mikeveng73@gmail.com>
2026-08-04 22:36:23 +0300

    + 2px left for RGB

--- drivers/tv/tv.c
+++ drivers/tv/tv.c
@@ -347,10 +347,10 @@ void tv_init(const output_format_e output_format) {
 
     /*
      * Базовая поправка по захвату: сдвигаем активную область вправо.
-     * При H_len=792 получаем porch 139 отсчётов слева и 27 справа.
+     * При H_len=792 получаем porch 137 отсчётов слева и 29 справа.
      */
     v_mode.begin_img_shx =
-        (v_mode.H_len - v_mode.img_size_x) / 2 + 56;
+        (v_mode.H_len - v_mode.img_size_x) / 2 + 54;
 
     // Единственный режим RGB TV: прогрессивный растр Вектора.
     v_mode.N_lines = 312;
